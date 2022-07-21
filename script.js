1e2f50f048c273329989951c57388e37cffe3c82
Devon-Henry <108783601+Capz-DH@users.noreply.github.com>
2022-07-21 15:39:18 +0100

    Add files via upload
    operational drop down menus

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-const features = document.getElementById("features")
-features.addEventListener("click", )
 
 
 function featuresDropdown() {
-    
+    document.getElementById("myDropdownContainer").classList.toggle("dropdown-contents-container")
+
 }
+
+
+function companyDropdown() {
+    document.getElementById("companyDropdownContainer").classList.toggle("company-dropdown-container")
+
+}
+
